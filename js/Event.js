a49d1c51304b93650053d1cc2d3a8c192f8ea5b3
Shivkesh176 <shivkesh.yadav_cs19@gla.ac.in>
2021-10-16 20:18:50 +0530

    created

---
+++ js/Event.js
@@ -0,0 +1,14 @@
+
+
+function fun1(){
+    var x=document.getElementById("container");
+    // alert("JAVASCRIPT")
+    x.style.backgroundColor = "blue"
+    x.style.border = "3px solid black"
+}
+function fun2(){
+    var x=document.getElementById("container");
+    // alert("JAVASCRIPT")
+    x.style.backgroundColor = "yellow"
+    x.style.border = "none"
+}
